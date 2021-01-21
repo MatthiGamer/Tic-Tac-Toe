f2e41a02c2a9a7e7ea79da9c5cedbd4e95839961
Matthias Bagehorn <matthigamer@gmail.com>
2021-01-21 15:59:03 +0100

    Reset added

--- Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Program.cs
@@ -8,8 +8,12 @@ namespace Tic_Tac_Toe
 {
     class Program
     {
+        private static bool[,] fields = new bool[3,3];
+
         static void Main(string[] args)
         {
+            ResetFields();
+
             Console.WriteLine("|---|---|---|");
             Console.WriteLine("| X | X | X |");
             Console.WriteLine("|---|---|---|");
@@ -17,6 +21,17 @@ namespace Tic_Tac_Toe
             Console.WriteLine("|---|---|---|");
             Console.WriteLine("| X | X | X |");
             Console.WriteLine("|---|---|---|");
+        }
+
+        private static void ResetFields()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    fields[i,j] = false;
+                }
+            }
         }
     }
 }
